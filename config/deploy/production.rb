0db5b195e81fc22b4462be595cd2e43528cf906d
rails <rails@rails>
2015-02-19 16:45:19 +0530

    small changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :domain, "23.227.172.79"
 set :rails_env, 'production'
 set :gitrepo, "git@github.com:neeraji2it/MediaCloud.git"
-set :deploydir, "/var/www/http://mediacloud.co.in/public_html"
+set :deploydir, "/var/www/mediacloud.co.in/public_html"
 set :user, "shivraj"
 set :use_sudo, false
