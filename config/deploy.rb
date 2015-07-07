700f0c88109db3d75f528fb19a443fc16886b3e0
rails <rails@rails.com>
2015-07-07 19:30:38 +0530

    correct project name

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'touchsecurecrafts'
+set :application, 'mediacloud'
 set :repo_url, 'git@github.com:neeraji2it/MediaCloud.git'
 
 # Default branch is :master
