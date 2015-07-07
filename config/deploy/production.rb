0bc0b3dab4f8e5f3ec3c1424887cf58458d8626f
rails <rails@rails.com>
2015-07-07 19:31:43 +0530

    correct project deployment path

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ role :db,  %w{shivraj@23.227.172.79}
 server '23.227.172.79', user: 'shivraj', roles: %w{web app db} #, my_property: :my_value
 
 set :domain, '23.227.172.79'
-set :deploy_to, '/var/www/touchsecurecrafts.com/public_html'
+set :deploy_to, '/var/www/mediacloud.co.in/public_html'
 set :branch, 'master'
 set :deploy_user, 'shivraj'
 set :log_level, :debug
